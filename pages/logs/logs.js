e38a2add3405e3ac95fe8ef65453bcc592345119
huangshuai <786262416@qq.com>
2020-12-03 00:36:39 +0800

    1

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,25 @@
-//logs.js
-const util = require('../../utils/util.js')
-
+const chelaile = requirePlugin("chelaile")
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
-})
+    data: {
+        mCanUse: false,
+        chelaileUrl: ''
+    },
+    onLoad: function () {
+        if (chelaile.caniuse()) {
+            let url = chelaile.getNavigatorUrl({
+                plugin: 'chelaile', // 用户定义的插件名字
+                lng: 116.410288, // 经纬度
+                lat: 39.995941,
+                vendor: '请联系商务获取' // 来源
+            });
+            this.setData({
+                mCanUse: true,
+                chelaileUrl: url
+            })
+        } else {
+            this.setData({
+                mCanUse: false
+            })
+        }
+    }
+});
